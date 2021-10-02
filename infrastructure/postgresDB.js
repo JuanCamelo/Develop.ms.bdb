1164cc0df2771da83155264b38a071f94c116a0f
Juan Pablo C <juanpcamelo01@gmail.com>
2021-10-01 21:22:32 -0500

    coenexión db

--- infrastructure/postgresDB.js
+++ infrastructure/postgresDB.js
@@ -7,8 +7,8 @@ exports.initPoolDB = () => {
         user: process.env.USER_DB,
         host: process.env.HOST_DB,
         database: process.env.NAME_DB,
-        password: process.env.PASS_DB,
-        port: process.env.PORT_DB,
+        password: process.env.PORT_DB,
+        port: process.env.PASS_DB,
     });
     exports.DBConnection = pool;
 };
