c86a2d6ab41481842c826f74d8ab17deaf18eaa9
Juan Pablo C <juanpcamelo01@gmail.com>
2021-10-01 20:13:06 -0500

    configuracion de la arquitectura, componentes y paquetes

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const logger = require("./config/logsConfig");
+const swaggerJsDoc = require("swagger-jsdoc");
+const db = require("./infrastructure/postgresDB");
+const dotenv = require("dotenv");
+//routes
+const employeesRoute = require("./routes/employeeRoutes");
+
+
+
+
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+};
+
+const app = express();
+db.initPoolDB();
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+
+//app.use(logger.middleware);
+app.get("/", (request, response) => {
+    response.json({ info: "Develop Module service is running..." });
+});
+
+//cofig Swagger
+const swaggerOptions = {
+    definition: {
+        openapi: '1.0.0',
+        info: {
+            title: 'Desarrollo de modulo de servicios de prueba BdB',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+/* Routes middleware */
+app.use(employeesRoute);
+
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry, can't find that!");
+});
+
+/* Start the server */
+var server = app.listen(process.env.PORT || 8080, () => {
+    console.log(`The serve starts at  http://localhost:${process.env.PORT}`);
+});
+
+process.on("SIGTERM", () => {
+    db.endPoolDB();
+    console.info("SIGTERM signal received.");
+    server.close();
+});
+
+process.on("SIGINT", () => {
+    db.endPoolDB();
+    console.info("SIGINT signal received.");
+    server.close();
+});
---
+++ routes/employeeRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const adCityController = require("../controllers/adCityController");
+const router = express.Router();
+
+/**
+   * @swagger
+   * /compras:
+   *   get:
+   *     description: Returns compras
+   *     tags:
+   *      - Compras
+   *     produces:
+   *      - application/json
+   *     responses:
+   *       200:
+   *         description: compras
+*/
+
+router.get('/employee',); 
+
+
+module.exports = router;
