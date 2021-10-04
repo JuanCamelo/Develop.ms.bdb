f867f43f5b6dc6145ab4fdfea687b0355686b356
Juan Pablo C <juanpcamelo01@gmail.com>
2021-10-03 22:39:38 -0500

    pruebas unitaria con mocha, chai

--- test/employee-tests.js
+++ test/employee-tests.js
@@ -1,8 +1,8 @@
-var chai = require('chai');
-var assert = chai.assert;
-var should = chai.should();
-var expect = chai.expect;
-var employeeDTO = require('../infrastructure/models/employeDTO');
+const chai = require('chai');
+const assert = chai.assert;
+const employeeDTO = require('../infrastructure/models/employeDTO');
+const employeeUpdateDTO = require('../infrastructure/models/employeUpdateDTO');
+const queries = require("../infrastructure/queries/getEmployeeQueriesModule");
 
 describe('Testing assert function: ', function() {
   describe('Check addTest Function', function(){
@@ -11,5 +11,33 @@ describe('Testing assert function: ', function() {
        assert.equal(result, result);
     });
   });
-})
+});
 
+describe('Testing assert function: ', function() {
+  describe('Check addTest Function', function(){
+    it('Check the returned value using : assert.equal(value, value): ', function() {
+      const param1 = "carllos"
+      const param2 = "emp" 
+       result = employeeUpdateDTO(param1, param2);
+       assert.equal(result, result);
+    });
+  });
+});
+
+describe('Testing assert function: ', function() {
+  describe('Check addTest Function', function(){
+    it('Check the returned value using : assert.equal(value, value): ', function() {
+       result = queries.getEmployeeById("31d7e785-575d-41b5-8ab9-22eeb42a373f");
+       assert.equal(result,result);
+    });
+  });
+});
+
+describe('Testing assert function: ', function() {
+  describe('Check addTest Function', function(){
+    it('Check the returned value using : assert.equal(value, value): ', function() {
+       result = queries.getEmployee("31d7e785-575d-41b5-8ab9-22eeb42a373f");
+       assert.equal(result, result);
+    });
+  });
+});
